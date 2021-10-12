fd93c6c9965bec800ca17a91098de060199275e5
sangmok <use0504@naver.com>
2021-10-12 23:00:31 +0900

    no message

--- hi.py
+++ hi.py
@@ -5,9 +5,10 @@ font_path = "C:/Windows/Fonts/NGULIM.TTF"
 font = font_manager.FontProperties(fname=font_path).get_name()
 rc('font', family=font)
 
+df_avg = pd.read_csv('c:/big-data/carbon 20210900.csv', encoding = 'cp949', index_col=0)
+df_my_index = pd.read_csv('c:/big-data/df_avg.csv', index_col=0, encoding = 'cp949', thousands = ',')
 
-df_my_index = pd.read_csv('c:/big-data/carbon point 20210900.csv', index_col=0, encoding = 'cp949', thousands = ',')
 
 df_my_index.plot(kind='bar', color=('b', 'r', 'g'))
-
+df_avg.plot(kind='bar', color=('g'))
 plt.show()
