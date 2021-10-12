6a756cea625518263c16b8e2c9b304b49cb39a6e
sangmok <use0504@naver.com>
2021-10-12 21:36:01 +0900

    한글 깨짐수정

--- hi.py
+++ hi.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+font_path = "C:/Windows/Fonts/NGULIM.TTF"
+font = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font)
 
 
-df_my_index = pd.read_csv('c:/big-data/carbon point 20200831.csv', index_col=0, encoding = 'cp949')
+df_my_index = pd.read_csv('c:/big-data/carbon point 20210900.csv', index_col=0, encoding = 'cp949')
 
-df_my_index['합계 (세대수)'].plot(kind='bar', color=('b', 'darkorange', 'g', 'r', 'm'))
+df_my_index['참여율(%)'].plot(kind='bar', color=('b', 'darkorange', 'g', 'r', 'm'))
 plt.show()
 
