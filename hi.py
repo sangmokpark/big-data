414640f360da266c79d69f84985459d347f919e3
sangmok <use0504@naver.com>
2021-10-12 17:49:18 +0900

    no message

--- hi.py
+++ hi.py
@@ -1,2 +1,10 @@
-print(hi)
-print("나 7시에 동아리 회의있어")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+countries_df = pd.read_csv(c:/big-data/)
+data = csv.reader(f)
+for row in data:
+  print(row[0], row[1])
+
+f.close()
