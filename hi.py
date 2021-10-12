19669534522893de43856b123f95a3f655ab855e
sangmok <use0504@naver.com>
2021-10-12 22:29:48 +0900

    ,사용가능하게 변경

--- hi.py
+++ hi.py
@@ -6,8 +6,8 @@ font = font_manager.FontProperties(fname=font_path).get_name()
 rc('font', family=font)
 
 
-df_my_index = pd.read_csv('c:/big-data/carbon point 20210900.csv', index_col=0, encoding = 'cp949')
-df_my_index = pd.to_numeric(df_my_index)
+df_my_index = pd.read_csv('c:/big-data/carbon point 20210900.csv', index_col=0, encoding = 'cp949', sep = ',', thousands = ',')
+
 df_my_index['참여가구'].plot(kind='bar', color=('b'))
 
 plt.show()
