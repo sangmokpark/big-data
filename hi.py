d6dae73804ba1333440de03a0ae9fc92a8718789
sangmok <use0504@naver.com>
2021-10-12 22:30:12 +0900

    no message

--- hi.py
+++ hi.py
@@ -6,7 +6,7 @@ font = font_manager.FontProperties(fname=font_path).get_name()
 rc('font', family=font)
 
 
-df_my_index = pd.read_csv('c:/big-data/carbon point 20210900.csv', index_col=0, encoding = 'cp949', sep = ',', thousands = ',')
+df_my_index = pd.read_csv('c:/big-data/carbon point 20210900.csv', index_col=0, encoding = 'cp949', thousands = ',')
 
 df_my_index['참여가구'].plot(kind='bar', color=('b'))
 
