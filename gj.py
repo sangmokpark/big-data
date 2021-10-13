56e5ec4f5c9c1f8f632ee385642ace28b2ad27cf
sangmok <use0504@naver.com>
2021-10-13 12:30:12 +0900

    TypeError: 'LinearRegression' object is not callable

--- gj.py
+++ gj.py
@@ -4,5 +4,15 @@ from matplotlib import font_manager, rc
 from sklearn import linear_model
 Carbon_Prediction = linear_model.LinearRegression()
 
+
+df_y = pd.read_csv('c:/big-data/carbon 20210900 gj.csv', encoding = 'cp949', index_col=0)
 x = [[2019], [2020], [2021]]
-y = []
+y = df_y['참여율(%)']
+Carbon_Prediction.fit(x,y)
+
+plt.scatter(x, y, color='black')
+
+y_pred =Carbon_Prediction(x)
+
+plt.plot(x, y_pred, color = 'blue', linewidth=3)
+plt.show()
